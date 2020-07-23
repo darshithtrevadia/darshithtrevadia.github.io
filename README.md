2cb3e454fdbe43cf5f9a1a19a1a486b4e2ac04db
Darshit Trevadia <darshithtrevadia@gmail.com>
2020-07-23 18:55:21 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# darshithtrevadia.github.io
---
+++ precache-manifest.c85e8119599af080c5595c1c4ae748ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf9b931e251522e21cb6e617e338af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "24b677cc93184de6f1b0",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b677cc93184de6f1b0",
+    "url": "/static/js/main.621a5cb6.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "1c32d785398e3a7eaab0e9b876903cc6",
+    "url": "/static/media/resume.1c32d785.pdf"
+  }
+]);
